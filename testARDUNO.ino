5a84bee25a290af40c9ae34f82b3ffbfa3153f1e
joseAveleira <jose.aveleira@gmail.com>
2025-12-10 19:19:35 +0100

    enciende y apaga leds

--- testARDUNO.ino
+++ testARDUNO.ino
@@ -1,10 +1,12 @@
 void setup() {
   Serial.begin(9600);
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
   Serial.println("Adios mundo cruel");
-  //pausa de 2 segundos
-
-  delay(2000);
+  digitalWrite(2, HIGH);
+  delay(1000);
+  digitalWrite(2, LOW);
+  delay(1000);
 }
